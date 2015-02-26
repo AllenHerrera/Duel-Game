3bdd046eb6191be0bba71167e37f90c9cff51611
Toby <Tobydgosselin@gmail.com>
2015-02-26 06:03:50 -0500

    modified server code and unity code to create demo

---
+++ UnityProject/Assets/challengeButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class challengeButton : MonoBehaviour {
+    public string challengeCode{get;set;}
+    private sockettest socket;
+    private void Start(){
+        socket = FindObjectOfType<sockettest>();
+    }
+    public void SubmitChallenge()
+    {
+        socket.challenge(challengeCode);
+    }
+}
