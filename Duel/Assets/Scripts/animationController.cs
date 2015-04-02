859df31d6c77f8c3587a5074bd64e7d1688e255f
bvalyan <bvalyan@hotmail.com>
2015-04-02 11:00:30 -0400

    Bug fix on animationController
    Temporarily disabled control of animations on keypress.
    Shortened code to simply work with other game controllers now
    Fixed syntax issues.

---
+++ Duel/Assets/Scripts/animationController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class animationController : MonoBehaviour {
+	GameObject duelFigure;
+	Animation duelAnim;
+	bool clicked = false;
+
+	public enum chooseAnimations {Idle, Shoot, Dead, Jam};
+
+
+	public IEnumerator animationChoice(chooseAnimations anim){
+
+		switch (anim) {
+		case chooseAnimations.Idle:
+			this.gameObject.GetComponent<Animation> ().Play ("Idle");
+			break;
+		case chooseAnimations.Jam:
+			this.gameObject.GetComponent<Animation> ().Play ("Jam");
+		case chooseAnimations.Shoot:
+			this.gameObject.GetComponent<Animation> ().Play ("Shoot");
+			yield return new WaitForSeconds (.5f);
+			this.gameObject.GetComponent<Animation> ().Play ("dance");
+		case chooseAnimations.Dead:
+			this.gameObject.GetComponent<Animation> ().Play ("Dead");
+	
+		}
+	}
+
+
+	// Use this for initialization
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+		}
+
+	}
