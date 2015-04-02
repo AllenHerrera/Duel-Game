8efe6ef4f018ffa959bfc7212443c1c4a88b8946
bvalyan <bvalyan@hotmail.com>
2015-04-02 13:08:36 -0400

    UI Test
    Added unfinished UI testing script.

---
+++ Duel/Assets/Editor/uiTest.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using NUnit.Framework;
+
+public class uiTest{
+	[Test]
+	public void postGamePanelTest(){
+		uiController.instance.ShowPanel (uiController.instance.PostGamePanel);
+		Assert.That (uiController.instance.PostGamePanel.enabled == true);
+	}
+	[Test]
+	public void challengingGamePanelTest(){
+		uiController.instance.ShowPanel (uiController.instance.PostGamePanel);
+		Assert.That (uiController.instance.ChallengingPanel.enabled == true);
+	}
+	[Test]
+	public void mainGamePanelTest(){
+		uiController.instance.ShowPanel (uiController.instance.MainPanel);
+		Assert.That (uiController.instance.MainPanel.enabled == true);
+	
+	}
+}	
