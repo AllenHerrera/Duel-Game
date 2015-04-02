593211f09dcc20701303daacd0fe9079d43f0e18
bvalyan <bvalyan@hotmail.com>
2015-04-02 13:43:34 -0400

    Finished UI Test Coding
    Added the rest of the panels to the UI test

--- Duel/Assets/Editor/uiTest.cs
+++ Duel/Assets/Editor/uiTest.cs
@@ -19,4 +19,47 @@ public class uiTest{
 		Assert.That (uiController.instance.MainPanel.enabled == true);
 	
 	}
+	[Test]
+	public void challengedPanelTest(){
+		uiController.instance.ShowPanel (uiController.instance.ChallengedPanel);
+		Assert.That (uiController.instance.ChallengedPanel.enabled == true);
+		
+	}
+	[Test]
+	public void drawPanelTest(){
+		uiController.instance.ShowPanel (uiController.instance.DrawPanel);
+		Assert.That (uiController.instance.DrawPanel.enabled == true);
+		
+	}
+	[Test]
+	public void preGamePanelTest(){
+		uiController.instance.ShowPanel (uiController.instance.PreGamePanel);
+		Assert.That (uiController.instance.PreGamePanel.enabled == true);
+		
+	}
+	[Test]
+	public void failPanelTest(){
+		uiController.instance.ShowPanel (uiController.instance.FailPanel);
+		Assert.That (uiController.instance.FailPanel.enabled == true);
+		
+	}
+	[Test]
+	public void optionsPanelTest(){
+		uiController.instance.ShowPanel (uiController.instance.OptionsPanel);
+		Assert.That (uiController.instance.OptionsPanel.enabled == true);
+		
+	}
+	[Test]
+	public void firstLoadPanelTest(){
+		uiController.instance.ShowPanel (uiController.instance.OnFirstLoadPanel);
+		Assert.That (uiController.instance.OnFirstLoadPanel.enabled == true);
+		
+	}
+	[Test]
+	public void changeNamePanelTest(){
+		uiController.instance.ShowPanel (uiController.instance.ChangeNamePanel);
+		Assert.That (uiController.instance.ChangeNamePanel.enabled == true);
+		
+	}
 }	
+
