dd5ff32ea4a15dc625cf7c854d7960f4ca712ec9
bvalyan <bvalyan@hotmail.com>
2015-04-12 14:04:56 -0400

    Updated Animation Controller
    Updated animation controller to support new costumes

--- Duel/Assets/Scripts/animationController.cs
+++ Duel/Assets/Scripts/animationController.cs
@@ -5,6 +5,11 @@ public class animationController : MonoBehaviour {
 	GameObject duelFigure;
 	Animation duelAnim;
 	bool clicked = false;
+	int hatSetting;
+	private SpriteRenderer torso, legs, hat;
+	public Sprite[] torsoOptions = new Sprite[4];
+	public Sprite[] legsOptions = new Sprite[4];
+	public Sprite[] hatsOptions = new Sprite[4];
 
 	public enum chooseAnimations {Idle, Shoot, Dead, Jam};
 
@@ -37,4 +42,19 @@ public class animationController : MonoBehaviour {
 	
 		}
 
+
+private void Start(){
+		torso = GameObject.FindGameObjectWithTag ("shirt").GetComponent<SpriteRenderer>();
+		hat = GameObject.FindGameObjectWithTag ("hats").GetComponent<SpriteRenderer>();
+		legs = GameObject.FindGameObjectWithTag ("legs").GetComponent<SpriteRenderer>();
 	}
+public void setHat(int option){
+		hat.sprite = hatsOptions[option]
+	}
+public void setShirt(int option){
+		torso.sprite = torsoOptions[option]
+	}
+public void setLegs(int option){
+		legs.sprite = legsOptions[option]
+	}
+}
