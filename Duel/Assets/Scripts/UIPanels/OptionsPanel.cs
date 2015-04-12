11d507b183b70ff77071138736cc79f1d363b569
AllenHerrera <aherre4@emory.edu>
2015-04-12 13:23:41 -0400

    fixed bugs with playerprefs and firstloadpanel

--- Duel/Assets/Scripts/UIPanels/OptionsPanel.cs
+++ Duel/Assets/Scripts/UIPanels/OptionsPanel.cs
@@ -8,93 +8,93 @@ public class OptionsPanel : menuPanel {
 	public Slider musicSlider;
 	public Slider volumeSlider;
 	public Scrollbar soundScrollbar;
-
+	
 	public float characterIndex;
 	public float music;
 	public float volume;
-
+	
 	public bool soundmute;
 	public bool updateScrollBar = false;
 	public bool PreviousScrollerState = false;
 	public float PreviousValue = 1;
 	public int count = 0;
-
+	
 	public string playerName;
 	public Text PlayerName;
 	public Text volumeText;
 	public Text soundText;
-
+	
 	public Sprite[] characterSprites;
 	public Sprite currentSprite;
-
+	
 	public AudioClip[] audioClips;
 	public AudioClip currentAudioClip;
 	
 	// Use this for initialization
 	void Start () {
-
-		Debug.Log(PlayerPrefs.GetFloat("savedVolume"));
-		Debug.Log(PlayerPrefs.GetFloat("savedSound"));
-		Debug.Log(PlayerPrefs.GetFloat("savedCharacter"));
-		Debug.Log(PlayerPrefs.GetFloat("savedMusic"));
+		
+		Debug.Log("savedVolume: " + PlayerPrefs.GetFloat("savedVolume"));
+		Debug.Log("savedSound: " + PlayerPrefs.GetFloat("savedSound"));
+		Debug.Log("savedCharacter: " + PlayerPrefs.GetFloat("savedCharacter"));
+		Debug.Log("savedMusic: " + PlayerPrefs.GetFloat("savedMusic"));
 		Debug.Log ("Playername is: " + PlayerPrefs.GetString ("playerProfile"));
-
+		
 		GameObject.FindWithTag("PlayerName").GetComponent<Text>().text = PlayerPrefs.GetString("playerProfile");
 		setDynamicSliders ();
-
+		
 		base.Start();
-
+		
 		loadSavedPlayerPrefs ();
 		_UpdateALL ();
 	}
-
+	
 	public void setDynamicSliders ()
 	{
-
+		
 		audioClips = Resources.LoadAll<AudioClip> ("Audio");
 		characterSprites = Resources.LoadAll<Sprite> ("Sprites");
-
+		
 		musicSlider.GetComponent<Slider> ().maxValue = audioClips.Length -1;
 		characterSlider.GetComponent<Slider> ().maxValue = characterSprites.Length-1;
 	}
-
+	
 	public void loadSavedPlayerPrefs ()
 	{
 		playerName = PlayerPrefs.GetString ("playerProfile");
 		//get saved music
-		music= PlayerPrefs.GetInt("savedMusic");
-		if (music == 0) 
-			{
-				music = 50;
-			}
-		Debug.Log ("here is the value of music:  " + music);
+		music= PlayerPrefs.GetFloat("savedMusic");
+		if (music == null) 
+		{
+			music = 0;
+		}
+		
 		musicSlider.value = music;
-
+		
 		//get saved mute value
-		if (PlayerPrefs.GetInt ("savedSound") == 0 || PlayerPrefs.GetInt ("savedSound") > 1) {
+		if (PlayerPrefs.GetFloat ("savedSound") == 0 || PlayerPrefs.GetFloat ("savedSound") > 1) {
 			soundmute = false;
 			soundScrollbar.value = 1;
 		} else {
 			soundmute = true;
 			soundScrollbar.value = 0;
 		}
-
+		
 		//get saved volume
-		volume = PlayerPrefs.GetInt ("savedVolume");
+		volume = PlayerPrefs.GetFloat ("savedVolume");
 		if (volume == 0) 
-			{
-				volume = 50;
-			}
+		{
+			volume = 50;
+		}
 		volumeSlider.value = volume;
-
+		
 		//get saved character
-		characterIndex = PlayerPrefs.GetInt("savedCharacter");
+		characterIndex = PlayerPrefs.GetFloat("savedCharacter");
 		if (characterIndex == 0) 
-			{
-				characterIndex = 1;
-			}
+		{
+			characterIndex = 1;
+		}
 		characterSlider.value = characterIndex;
-
+		
 	}
 	public void _UpdateALL()
 	{
@@ -103,92 +103,94 @@ public class OptionsPanel : menuPanel {
 		UpdateVolume ();
 		UpdateSprite ();
 	}
-
+	
 	public void UpdateVolume()
 	{	
-
+		
 		volume = volumeSlider.value;
 		volumeText.text = " Volume: " +volumeSlider.value.ToString ();
 		GameObject.FindGameObjectWithTag ("BackgroundMusic").GetComponent<AudioSource> ().volume = (volume / 100);
 	}
-
+	
 	public void UpdateSound()
 	{ 	updateScrollBar = true;
 		count = 10;
-
+		
 		if (soundScrollbar.value <= 0.5) 
 		{	
 			PlayerPrefs.SetFloat ("savedSound", 1);
 			soundmute = true;
 			soundText.text = "Music: Off";
-		
+			
 		}
 		if (soundScrollbar.value >= 0.5) 
 		{
 			PlayerPrefs.SetFloat ("savedSound", 2);
 			soundmute = false;
 			soundText.text = "Music: On";
-
+			
 		}
-
+		
 		GameObject.FindGameObjectWithTag ("BackgroundMusic").GetComponent<AudioSource> ().mute = soundmute;
 	}
 	public void UpdateMusic()
 	{	GameObject.FindGameObjectWithTag("OptionBtnSound").GetComponent<AudioSource>().Play();
-
+		
 		music = musicSlider.value;
 		currentAudioClip = audioClips [(int)music];
-		GameObject.FindGameObjectWithTag ("BackgroundMusic").GetComponent<AudioSource> ().clip = currentAudioClip;
-		GameObject.FindGameObjectWithTag ("BackgroundMusic").GetComponent<AudioSource> ().Play();
-
+		GameObject gameObject = GameObject.FindGameObjectWithTag ("BackgroundMusic");
+		gameObject.GetComponent<AudioSource> ().clip = currentAudioClip;
+		gameObject.GetComponent<AudioSource> ().Play();
+		
 	}
-
+	
 	public void UpdateSprite ()
 	{	GameObject.FindGameObjectWithTag("OptionBtnSound").GetComponent<AudioSource>().Play();
-
+		
 		characterIndex = characterSlider.value;
 		currentSprite = characterSprites [(int)characterIndex];
-        
 		GameObject.FindWithTag ("CurrentSprite").GetComponent<SpriteRenderer> ().sprite = currentSprite;
-
+		
 	}
-
+	
 	protected override void ProcessButtonPress(ButtonAction btn)
 	{
 		switch (btn) {
 		case ButtonAction.returnToMain:
-		    {
+		{
 			GameObject.FindGameObjectWithTag("OptionBtnSound").GetComponent<AudioSource>().Play();
-
+			
 			GameObject.FindWithTag("CurrentSprite").GetComponent<SpriteRenderer> ().enabled = false;
 			GameObject.FindGameObjectWithTag ("CharacterSlider").GetComponent<RectTransform> ().DOAnchorPos (new Vector2 (-745, -420), 0.6f, true);
 			PlayerPrefs.SetFloat ("savedCharacter", characterIndex);
 			PlayerPrefs.SetFloat ("savedMusic", music);
 			PlayerPrefs.SetFloat ("savedVolume", volume);
-			Debug.Log ("Playername is: " + PlayerPrefs.GetString ("playerProfile"));
-				// transition to mainpanel
-			Debug.Log(PlayerPrefs.GetFloat("savedVolume"));
-			Debug.Log(PlayerPrefs.GetFloat("savedSound"));
-			Debug.Log(PlayerPrefs.GetFloat("savedCharacter"));
-			Debug.Log(PlayerPrefs.GetFloat("savedMusic"));
+			Debug.Log ("Playername going into main is: " + PlayerPrefs.GetString ("playerProfile"));
+			// transition to mainpanel
+			
 			uiController.instance.ShowPanel (uiController.instance.MainPanel);
-
-
-			}
+			
+			
+		}
 			break;
 		case ButtonAction.options:
 		{
 			GameObject.FindGameObjectWithTag("OptionBtnSound").GetComponent<AudioSource>().Play();
-
+			
 			GameObject.FindWithTag("CurrentSprite").GetComponent<SpriteRenderer> ().enabled = false;
 			GameObject.FindGameObjectWithTag ("CharacterSlider").GetComponent<RectTransform> ().DOAnchorPos (new Vector2 (-745, -420), 0.6f, true);
 			PlayerPrefs.SetFloat ("savedCharacter", characterIndex);
 			PlayerPrefs.SetFloat ("savedMusic", music);
 			PlayerPrefs.SetFloat ("savedVolume", volume);
-
-
+			
+			Debug.Log ("Playername going into changename: " + PlayerPrefs.GetString ("playerProfile"));
+			Debug.Log("savedVolume: " +PlayerPrefs.GetFloat("savedVolume"));
+			Debug.Log("savedSound: " +PlayerPrefs.GetFloat("savedSound"));
+			Debug.Log("savedcharacter: " +PlayerPrefs.GetFloat("savedCharacter"));
+			Debug.Log("savedMusic: " +PlayerPrefs.GetFloat("savedMusic"));
+			
 			uiController.instance.ShowPanel (uiController.instance.ChangeNamePanel);
-		
+			
 		}
 			break;
 		}
@@ -201,62 +203,62 @@ public class OptionsPanel : menuPanel {
 			// change from 1 to 0 or from 0 to 1
 			if (PreviousValue > 0.5 && value < 0.5 || PreviousValue < 0.5 && value >= 0.5)
 			{
-
-					PreviousScrollerState = ScrollerState;
-					PreviousValue = value;
-					return true;
-
+				
+				PreviousScrollerState = ScrollerState;
+				PreviousValue = value;
+				return true;
+				
 			}
 			else // no change in direction
 			{return false;
 			}
 		}
-
+		
 	}
 	
 	public void returnToMain()
 	{
 		ProcessButtonPress(ButtonAction.returnToMain);
 	}
-
+	
 	public void ToChangeNamePanel()
 	{
 		ProcessButtonPress(ButtonAction.options);
 	}
-	 void Update()
+	void Update()
 	{ 
-	
+		
 		if (count > 0) {
 			if (soundScrollbar.value > 0.5) {
 				
 				soundScrollbar.value = (soundScrollbar.value + ((1 - soundScrollbar.value) / 12));
 			} else {
 				soundScrollbar.value = soundScrollbar.value - (soundScrollbar.value) / 12;
-
+				
 			}
 			if(CheckIfChange(updateScrollBar,soundScrollbar.value)==true)
 				GameObject.FindGameObjectWithTag("OptionBtnSound").GetComponent<AudioSource>().Play();
 			count --;
 			if (count == 0)
 			{
-			if (soundScrollbar.value > 0.5) 
-				
-				soundScrollbar.value = 1;
-			else 
-				soundScrollbar.value = 0;
+				if (soundScrollbar.value > 0.5) 
+					
+					soundScrollbar.value = 1;
+				else 
+					soundScrollbar.value = 0;
 			}	
-
+			
 			updateScrollBar = false;
 		}
-
 		
-
 		
-
-
-
-
+		
+		
+		
+		
+		
+		
 	}
-
-
+	
+	
 }
