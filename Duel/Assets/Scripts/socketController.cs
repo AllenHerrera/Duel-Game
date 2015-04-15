ff35b0dbb7116bb74d3aa7527837369c825bf0e5
= <Tobydgosselin@gmail.com>
2015-04-15 02:18:56 -0400

    added leaderboard querying

--- Duel/Assets/Scripts/socketController.cs
+++ Duel/Assets/Scripts/socketController.cs
@@ -83,45 +83,7 @@ public class socketController : MonoBehaviour
         string data = string.Format("{0}", e.data["leaderboard"]);
         JSONObject leaderboard = new JSONObject(data);
         Debug.Log((leaderboard[0]));
-        //accessData(JSON);
-    
-        
     }
-    void accessData(JSONObject obj)
-    {
-        switch (obj.type)
-        {
-            case JSONObject.Type.OBJECT:
-                for (int i = 0; i < obj.list.Count; i++)
-                {
-                    string key = (string)obj.keys[i];
-                    JSONObject j = (JSONObject)obj.list[i];
-                    Debug.Log(key);
-                    accessData(j);
-                }
-                break;
-            case JSONObject.Type.ARRAY:
-                foreach (JSONObject j in obj.list)
-                {
-                    accessData(j);
-                }
-                break;
-            case JSONObject.Type.STRING:
-                Debug.Log(obj.str);
-                break;
-            case JSONObject.Type.NUMBER:
-                Debug.Log(obj.n);
-                break;
-            case JSONObject.Type.BOOL:
-                Debug.Log(obj.b);
-                break;
-            case JSONObject.Type.NULL:
-                Debug.Log("NULL");
-                break;
-
-        }
-    }
-
     private void pingResult(SocketIOEvent e)
     {
        playerPing= int.Parse(string.Format("{0}", e.data["ping"]));
